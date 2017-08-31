0efc89bc38450d5cc79d895710517183032c0806
alonatramon <31254630+alonatramon@users.noreply.github.com>
2017-08-31 22:26:53 +0300

    tabs according to convention

--- GoJava.java
+++ GoJava.java
@@ -1,6 +1,7 @@
 public class GoJava {
-	public static void main (String[] args){
+    
+	public static void main (String[] args){	
 		System.out.println ("Hi, I'm your first Java program! Stay tuned!");
-	}
+	}	
 	
 }
